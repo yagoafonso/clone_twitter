7882943c290793183f981dcc2e8e16192a6edc1c
Yago Afonso <yago.afonso@hotmail.com>
2022-08-31 21:02:28 -0400

    Adicionado barra de pesquisa

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-
-
+import { MagnifyingGlass } from 'phosphor-react'
 import { 
   ContainerAside,
-
+  ContainerSearch,
+  ContentInput,
 } from "./style";
 export function Aside() {
   return (
     <ContainerAside>
-
+      <ContainerSearch>
+        <MagnifyingGlass />
+        <ContentInput type="text" placeholder="Search Twitter"></ContentInput>
+      </ContainerSearch>
+      <ContainerTopTopics>
+        <h1>What´s happening</h1>
+      </ContainerTopTopics>
     </ContainerAside>
+    
 
   );
 }
